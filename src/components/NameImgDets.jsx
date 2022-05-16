799a6a9d3e397211fdf54ffe7b4a3633c5475121
Khushi Mehta <khushimehta8282@gmail.com>
2022-05-16 10:39:53 +0530

    fixed name and rank

--- src/components/NameImgDets.jsx
+++ src/components/NameImgDets.jsx
@@ -20,7 +20,7 @@ function NameImgDets(props) {
             {
                 data ?
                     <Grid container sx={{ display: 'flex', alignItems: 'center', width: '100%', marginBottom: '2%' }}>
-                        <Grid item md={4} container sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Grid item md={4} sm={4} xs={4} container sx={{ display: 'flex', alignItems: 'center' }}>
                             <Grid item sx={{ backgroundColor: '#222430', marginRight: '1%', borderRadius: '50%', padding: '10px', width: '50px', height: '50px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                                 <img src={data.image.thumb} />
                             </Grid>
@@ -28,7 +28,7 @@ function NameImgDets(props) {
                                 <h3 style={{ fontFamily: 'Poppins', textTransform: 'capitalize', color: '#F1F1F1' }}>{data.name}</h3>
                             </Grid>
                         </Grid>
-                        <Grid item md={8} container sx={{ display: 'flex', width: '100%', justifyContent: 'flex-end' }}>
+                        <Grid item md={8} sm={8} xs={8} container sx={{ display: 'flex', width: '100%', justifyContent: 'flex-end' }}>
                             <Grid item md={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
                                 <Chip
                                     sx={{ color: '#F1F1F1', backgroundColor: '#222430' }}
